d0d3c12e153de53c27758d64afe9e71f45ed0607
Brian Partridge <bpartridge@Brians-MacBook-Air.local>
2013-01-06 22:47:08 -0800

    Work on the homepage

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 var express = require('express'),
-	app = express();
+	app = express(),
+	twig = require('twig');
+
+app.configure(function () {
+    app.use(express.static(__dirname + '/public'));
+	app.use(express.bodyParser());
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'html.twig');
+	app.engine('html.twig', twig.__express);
+    // We don't need express to use a parent "page" layout
+    // Twig.js has support for this using the {% extends parent %} tag
+    app.set("view options", { layout: false });
+});
 
 app.get('/*', function(req, res, next) {
   if (req.headers.host.match(/^www/) !== null ) {
@@ -9,11 +21,11 @@ app.get('/*', function(req, res, next) {
   }
 });
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(req, res) {
+  res.render('index', {
+	message: 'testing 123'
+  });
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(port);
