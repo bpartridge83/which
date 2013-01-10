f5e3d9f51fd2808bc2b0c2a5891580a6c6eecac9
Brian Partridge <bpartridge83@gmail.com>
2013-01-09 23:08:28 -0800

    Testing webhook again

--- web/app.js
+++ web/app.js
@@ -9,6 +9,7 @@ var app = function (app, express, argv) {
 		app.use(express.favicon(__dirname + '/../public/favicon.ico', {maxAge: 86400000}));
 	    app.use(express.static(__dirname + '/../public'));
 		app.use(express.bodyParser());
+		
 	    app.set('views', __dirname + '/../views');
 	    app.set('view engine', 'html.twig');
 		app.engine('html.twig', twig.__express);
