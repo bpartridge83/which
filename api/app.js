af52772937780aaf5a318f60ea5e39cb6896c309
Brian Partridge <bpartridge83@gmail.com>
2013-01-22 20:52:59 -0800

    Trying to get the API working

--- api/app.js
+++ api/app.js
@@ -6,11 +6,13 @@ var app = function (app, express, argv) {
 		bcrypt = require('bcrypt-nodejs'),
 		mongo = require('mongoskin'),
 		db = mongo.db(conf.db),
+		md5 = require('MD5'),
 		model = require('../common/models')(db),
 		repo = require('../common/repositories')(db, model);
 	
 	app.configure(function () {	
 		app.use(express.compress());
+		app.use(express.bodyParser());
 		app.use(express.static(__dirname + '/../public'));
 	});
 	
@@ -21,8 +23,11 @@ var app = function (app, express, argv) {
 		var user = new model.User({
 			username: req.body.username,
 			password: bcrypt.hashSync(req.body.password),
-			token: MD5(username + time)
+			token: md5(req.body.username + time)
 		});
+		
+		console.log(req.body.username);
+		console.log(req.body.password);
 		
 		user.save(function () {
 			return res.send(user.toJSON());
